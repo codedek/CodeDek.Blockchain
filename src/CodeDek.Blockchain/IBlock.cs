6733915672d1876ef5d0442304eaaf0cdf917714
codedek <codedek@outlook.com>
2019-01-07 16:07:46 -0400

    Add IBlock Interface

---
+++ src/CodeDek.Blockchain/IBlock.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeDek.Blockchain
+{
+    public interface IBlock
+    {
+        ulong Index { get; }
+        ulong Nonce { get; }
+        string Data { get; }
+        string Hash { get; }
+        string PreviousHash { get; }
+        string Timestamp { get; }
+        string CalculateHash();
+    }
+}
